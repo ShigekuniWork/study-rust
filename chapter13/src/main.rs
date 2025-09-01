073ad12f5db5952c575c43dfc0116d024687c844
ShigekuniWork <shigekuni.work@gmail.com>
2025-09-02 00:01:52 +0900

    chapter13

---
+++ chapter13/src/main.rs
@@ -0,0 +1,85 @@
+use chapter13::{Shoe, shoes_in_my_size};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    let map_vec: Vec<i32> = vec![1, 2, 3];
+    let map_vec2: Vec<_> = map_vec.iter().map(|x| x + 1).collect();
+
+    let shoes = vec![Shoe {
+        size: 1,
+        style: "style".to_string(),
+    }];
+    let shoe_size = shoes_in_my_size(shoes, 1);
+
+    println!("{:?}", shoe_size);
+    assert_eq!(map_vec2, map_vec);
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+
+        num
+    });
+
+    if intensity < 25 {
+        println!(
+            // 今日は{}回腕立て伏せをしてください！
+            "Today, do {} pushups!",
+            expensive_result.value(intensity)
+        );
+
+        println!(
+            // 次に、{}回腹筋をしてください！
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
+    } else if random_number == 3 {
+        // 今日は休憩してください！水分補給を忘れずに！
+        println!("Take a break today! Remember to stay hydrated!");
+    } else {
+        println!(
+            // 今日は、{}分間走ってください！
+            "Today, run for {} minutes!",
+            expensive_result.value(intensity)
+        );
+    }
+}
